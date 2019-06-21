27e7be0246114f4c415c9f1c3a0dd3708aa55d0c
tavisca-vshah <vshah@tavisca.in>
2019-06-21 13:11:21 -0700

    Problem Solved

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -16,14 +16,50 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         private static void Test(string args, int expected)
         {
-            var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
-            Console.WriteLine($"{args} : {result}");
+            var C = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
+            Console.WriteLine($"{args} : {C}");
         }
 
         public static int FindDigit(string equation)
-        {
-            // Add your code here.
-            throw new NotImplementedException();
-        }
-    }
+        {   
+            string C = equation.Split('=')[1];
+            string A = equation.Split('*')[0];
+            string B = equation.Split('*')[1].Split('=')[0];
+            float compResult = -1;
+            string givenResult = "";
+            
+
+            if(A.IndexOf('?') > -1){
+                compResult = ((float)Int32.Parse(C))/Int32.Parse(B);
+                givenResult = A;
+            }
+            else if(B.IndexOf('?') > -1){
+                compResult = ((float)Int32.Parse(C))/Int32.Parse(A);   
+                givenResult = B;
+            }
+            else if(C.IndexOf('?') > -1){
+                compResult = Int32.Parse(A)*Int32.Parse(B);
+                givenResult = C;
+            }
+
+           // computed result is not integer, no integer solution possible
+            if(compResult != (int)compResult){
+                return -1;
+            }
+
+            string temp = compResult.ToString();
+
+            if(temp.Length != givenResult.Length){
+                // length mismatch
+                return -1;
+            }
+            else{
+
+             int index=givenResult.IndexOf('?');
+             return (int)temp[index]-48;
+             
+         }       
+
+     }
+ }
 }
